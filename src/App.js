b4e11980d5a31d3c9af331047797887637288918
Robert Hammock <rhamm1027@gmail.com>
2021-04-09 17:38:01 -0400

    basic socket working

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ const ENDPOINT = "http://localhost:8080";
 
 class App extends React.Component {
 
-  // componentDidMount() {
-  //   const socket = socketIOClient(ENDPOINT);
-  //   socket.on('join-room', (socket) => {
-  //     console.log("Joined room");
-  //   })
-  // }
+  componentDidMount() {
+    const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
+    socket.emit('join-room', (socket) => {
+      console.log("Joined room");
+    })
+  }
 
   render() {
     return (
