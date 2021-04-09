0f60565f01c5f66b716639e3d43f52e82b512219
Robert Hammock <rhamm1027@gmail.com>
2021-04-09 18:14:29 -0400

    Wiring socket for new room

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import './App.css';
-import socketIOClient from 'socket.io-client';
 import { Route } from 'react-router';
 import Home from './Home/Home';
 import Room from './Room/Room';
-const ENDPOINT = "http://localhost:8080";
 
 class App extends React.Component {
-
-  componentDidMount() {
-    let roomId;
-    const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
-    socket.emit('join-room', (socket) => {
-      console.log("Joined room");
-    })
-    socket.on('uuid', (id) => {
-      roomId = id;
-      console.log(roomId);
-      
-    })
-  }
 
   render() {
     return (
--- src/Room/Room.js
+++ src/Room/Room.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Video from '../Video/Video';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = "http://localhost:8080";
+const socket = socketIOClient(ENDPOINT, {transports: ['websocket']});
 
-const Room = () => {
-    return (
-        <>
-            <Video />
-            <Link to='/'>Back to Home</Link>
-        </>
-    )
+class Room extends React.Component {
+
+    componentDidMount() {
+        const { roomId } = this.props.match.params;
+        
+        socket.emit('join-room', (socket) => {
+          console.log("Joined room");
+        })
+        socket.emit('uuid', roomId);
+    }
+
+    handleNewUserJoin = () => {
+        socket.on('user-connected', () => {
+            console.log('a new user has joined');
+        })
+    }
+
+
+    render() {
+        this.handleNewUserJoin();
+        
+        return (
+            <>
+                <Video />
+                <Link to='/'>Back to Home</Link>
+            </>
+        )
+    }
 }
 
 export default Room;
