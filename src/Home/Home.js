1ec1f84f1115a41205247663f77b761fbc9e3cc2
Robert Hammock <rhamm1027@gmail.com>
2021-04-09 19:01:14 -0400

    validate join room and wired class methods

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,23 +2,81 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import uuid from 'uuid';
 
-const handleNewRoom = () => {
-    // Link to a new room with a unique id
-    console.log("Time to start a new room boss")
-}
+class Home extends React.Component {
 
-const handleJoinRoom = () => {
-    // add text input to render so user can input id of room to join
-    console.log("Time to join a new room boss")
-}
+    state = {
+        join: false,
+        hasRoom: null,
+        error: null,
+    }
+    handleJoinRoom = async (event) => {
+        event.preventDefault();
+        const { roomId } = event.target;
 
-const Home = () => {
-    return (
-      <div className='button-container'>
-        <button onClick={handleJoinRoom} type="button">Join a room</button>
-        <Link to={`/${uuid()}`} onClick={handleNewRoom} type="button">Start a room</Link>
-      </div>
-    )
+        await this.handleRoomIdCheck(roomId.value)
+        
+        const { hasRoom } = this.state;
+        if(hasRoom) {
+            this.props.history.push(`/${roomId.value}`);
+        }
+        
+    }
+
+    handleRoomIdCheck = async (id) => {
+        await fetch('http://localhost:8080/rooms', {
+            headers: {
+                room_id: id,
+            }
+        }).then((res) => {
+            if (!res) {
+                return res.json().then((error) => this.setState(error));
+            } else {
+                return res.json();
+            }
+        }).then((resJson) => {
+            if (resJson) {
+                this.setState({ hasRoom: true });
+                                
+            } else {
+                this.setState({ hasRoom: false });
+                
+            }
+        }).catch((error) => {
+            this.setState(error)
+        });
+            
+    }
+
+    handleRoomId = () => {
+        // add text input to render so user can input id of room to join
+        console.log("Time to join a new room boss")
+        this.setState({ join: true });
+    }
+
+    render() {
+        const { join, hasRoom } = this.state;
+
+        return (
+            <div className='button-container'>
+              {(!join) 
+                ? <button onClick={this.handleRoomId} type="button">Join a room</button>
+                : (
+                    <>
+                        <form onSubmit={this.handleJoinRoom}>
+                            <label htmlFor='roomId'>Room ID: </label>
+                            <input id='roomId' name='roomId' type='text' />
+                            <button type='submit'>Submit</button>
+                        </form>
+                        
+                    </>
+                )}
+                {(hasRoom === false) ? <p>Sorry that Room Id is not valid</p> : null}
+              <Link to={`/${uuid()}`}>
+                  <button type='button'>Start a room</button>
+                  </Link>
+            </div>
+          )
+    }
 }
 
 export default Home;
